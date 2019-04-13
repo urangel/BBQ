82d8a3936ff89d316d1bce729a0a71fef4388a7c
Ulises Rangel <uli.atx@gmail.com>
2019-04-12 20:53:10 -0500

    introducing jaws_db

--- config/connection.js
+++ config/connection.js
@@ -2,32 +2,38 @@ const mysql = require("mysql");
 
 require("dotenv").config();
 const keys = require("./keys");
+let connection;
 
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    port     : 3306,
-    user     : 'root',
-    password : keys.secret,
-    database : 'bbq_db',
-    typeCast: function castField( field, useDefaultTypeCasting ) {
-
-        // We only want to cast bit fields that have a single-bit in them. If the field
-        // has more than one bit, then we cannot assume it is supposed to be a Boolean.
-        if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
-
-            var bytes = field.buffer();
-
-            // A Buffer in Node represents a collection of 8-bit unsigned integers.
-            // Therefore, our single "bit field" comes back as the bits '0000 0001',
-            // which is equivalent to the number 1.
-            return( bytes[ 0 ] === 1 );
-
+if (process.env.JAWSDB_URL) {
+    connection.mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host     : 'localhost',
+        port     : 3306,
+        user     : 'root',
+        password : keys.secret,
+        database : 'bbq_db',
+        typeCast: function castField( field, useDefaultTypeCasting ) {
+    
+            // We only want to cast bit fields that have a single-bit in them. If the field
+            // has more than one bit, then we cannot assume it is supposed to be a Boolean.
+            if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
+    
+                var bytes = field.buffer();
+    
+                // A Buffer in Node represents a collection of 8-bit unsigned integers.
+                // Therefore, our single "bit field" comes back as the bits '0000 0001',
+                // which is equivalent to the number 1.
+                return( bytes[ 0 ] === 1 );
+    
+            }
+    
+            return( useDefaultTypeCasting() );
+    
         }
-
-        return( useDefaultTypeCasting() );
-
-    }
-});
+    });
+}
 
 connection.connect( (err, res) =>{
     if(err) throw err;
